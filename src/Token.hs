afbb1492b600cf2472e7fabe43a9ecc2e0300fe3
Shant Hairapetian <shairap@sandia.gov>
2026-04-16 02:31:04 -0700

    codecrafters submit [skip ci]

--- src/Token.hs
+++ src/Token.hs
@@ -7,4 +7,4 @@ data Token = LeftParen | RightParen | EOF
 instance Show Token where
   show LeftParen  = "LEFT_PAREN ( null"
   show RightParen =  "RIGHT_PAREN ) null"
-  show EOF = "EOF null"
+  show EOF = "EOF  null"
