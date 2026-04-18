5fc0619ede7f9600daae3246cad7a3d160ea1a55
Shant Hairapetian <shairap@sandia.gov>
2026-04-17 21:26:13 -0700

    codecrafters submit [skip ci]

--- src/AST.hs
+++ src/AST.hs
@@ -43,7 +43,7 @@ expPos (Ident  n _)     = n
 
 instance Show (Exp n a) where
   show :: Exp n a -> String
-  show (EInt _ n)          = show n
+  show (EInt _ n)          = printf "%.1f" n
   show (EBinOp _ op e1 e2) = printf "(%s %s %s)" (show op) (show e1) (show e2)
   show (EBool _ True)      = "true"
   show (EBool _ False)     = "false"
