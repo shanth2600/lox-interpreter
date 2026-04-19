867234c2f608bc766d212b319d80ec326e1e6c2b
Shant Hairapetian <shairap@sandia.gov>
2026-04-19 00:39:34 -0700

    codecrafters submit [skip ci]

--- src/Interp.hs
+++ src/Interp.hs
@@ -2,11 +2,13 @@ module Interp where
 
 import AST ( Exp (..), Op (..))
 import Text.Parsec (SourcePos)
+import Parser (testParse)
 
 data Val = 
     VInt Int
   | VBool Bool
   | VNil
+  | VString String
   deriving Eq
 
 instance Show Val where
@@ -14,15 +16,18 @@ instance Show Val where
   show (VBool True)  = "true"
   show (VBool False) = "false"
   show (VNil)        = "nil"
+  show (VString str) = str
 
 eval :: Exp SourcePos -> Val
-eval (ENil _)     = VNil
-eval (EInt _ n)   = VInt n
-eval (EBool _ b)  = VBool b
-eval (ENot _ e) = case eval e of
-  VBool b -> VBool $ not b
-  _       -> error "type error"
-eval (ENeg _ n) = case eval n of
+eval (ENil _)            = VNil
+eval (EInt _ n)          = VInt n
+eval (EBool _ b)         = VBool b
+eval (EString _ str)     = VString str
+eval (ENot _ e)          = 
+  case eval e of
+    VBool b -> VBool $ not b
+    _       -> error "type error"
+eval (ENeg _ n)          = case eval n of
   VInt n' -> VInt (- n')
 eval (EBinOp _ op e1 e2) = 
   let v1 = eval e1
@@ -32,3 +37,7 @@ eval (EBinOp _ op e1 e2) =
     (Minus, (VInt v1'), (VInt v2')) -> VInt (v1' - v2')
     (Mult, (VInt v1'), (VInt v2')) -> VInt (v1' * v2')
     (Div, (VInt v1'), (VInt v2')) -> VInt (v1' `div` v2')
+
+
+testEval :: String -> String    
+testEval = show . eval . testParse
